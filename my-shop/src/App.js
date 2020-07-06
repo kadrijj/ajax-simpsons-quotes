b0bd481c86945afd145379a0ef0b6483ece200b5
Kadri Jarvpold <kadrijarvpold@Kadris-Air.home>
2020-07-06 10:08:50 +0100

    navbar

---
+++ my-shop/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Navbar from "./components/Navbar";
+import './App.css';
+
+class App extends React.Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return <Navbar />
+  }
+}
+
+export default App;
---
+++ my-shop/src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './Navbar.css';
+
+const Navbar = () => {
+    return (
+        <ul>
+            <li>HOME</li>
+            <li>SHOP</li>
+            <li>CONTACT US</li>
+        </ul>
+    );
+}
+
+export default Navbar;
