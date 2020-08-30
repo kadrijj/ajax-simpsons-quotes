0f1600493f28bdf854b4e43d7d3b32d586916578
Kadri Jarvpold <kadrijarvpold@Kadris-Air.home>
2020-08-30 13:13:51 +0100

    API done

--- my-shop/src/App.js
+++ my-shop/src/App.js
@@ -1,15 +1,25 @@
 import React, { Component } from 'react';
-import Navbar from "./components/Navbar";
+import { Switch, Route } from 'react-router-dom';
 import './App.css';
+import Navbar from "./Components/Navbar";
+import Home from "./Components/Home";
+import Shop from "./Components/Shop";
+import Contact from "./Components/Contact";
+import Footer from "./Components/Footer";
 
-class App extends React.Component {
-  constructor() {
-    super();
-  }
-
-  render() {
-    return <Navbar />
-  }
+const App = () => {
+  return (
+    <>
+      <Navbar />
+      <Switch>
+        <Route exact path='/' component={Home} />
+        <Route path='/shop' component={Shop} />
+        <Route path='/contact' component={Contact} />
+      </Switch>
+      <Footer />
+    </>
+  );
 }
 
 export default App;
+
--- my-shop/src/components/Footer.js
+++ my-shop/src/components/Footer.js
@@ -1,16 +1,14 @@
 import React from "react";
 import { NavLink } from 'react-router-dom';
 
-const Navbar = () => {
+const Footer = () => {
     return (
-        <ul>
-            <div className="title">Chido</div>
+        <ul className="footer">
             <li><NavLink activeClassName='active' exact to='/'>Home</NavLink></li>
             <li><NavLink activeClassName='active' to='/shop'>Shop</NavLink></li>
             <li><NavLink activeClassName='active' to='/contact'>Contact Us</NavLink></li>
         </ul>
     )
+
 }
-
-
-export default Navbar;
+export default Footer;
